7304343cd24b4bf3f5c3200c2a8616ec519480e5
Shianne <slesure1@ggc.edu>
2023-10-30 16:06:56 -0400

    added student button

--- app/src/main/java/com/example/testingbutton/MainActivity.kt
+++ app/src/main/java/com/example/testingbutton/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : ComponentActivity() {
                     //studentButton()
                     Greeting(name = "Shianne")
                     studentButton2()
+                    studentButton()
                 }
             }
         }
